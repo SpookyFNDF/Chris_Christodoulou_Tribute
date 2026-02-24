e42818f0e55bbfc037dc8244f177cdf33018278c
SpookyFNDF <fernando.d.ferreira2006@gmail.com>
2026-02-24 11:13:25 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 onmousemove = function(e)
 {
-    var cursor = document.getElementById("customCursor");
+    let cursor = document.getElementById("customCursor");
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
 }   
